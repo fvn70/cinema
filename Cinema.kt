ff59dace6f52e488cd3c2cc8b250e11c875d1cca
fvn70 <fvn_reg@mail.ru>
2022-01-21 21:59:08 +0700

    Cinema Room Manager. Stage 3/5: Tickets

--- Cinema.kt
+++ Cinema.kt
@@ -2,14 +2,24 @@ package cinema
 
 var rows = 7
 var cols = 8
+val seats by lazy { MutableList(rows) { MutableList(cols) { 'S' } } }
+
 
 fun main() {
     println("Enter the number of rows:")
     rows = readLine()!!.toInt()
     println("Enter the number of seats in each row:")
     cols = readLine()!!.toInt()
-    println("Total income:\n$${income()}")
-//    draw()
+    draw()
+
+    println("Enter a row number:")
+    val row = readLine()!!.toInt()
+    println("Enter a seat number in that row:")
+    val col = readLine()!!.toInt()
+
+    println("Ticket price: \$${price(row)}")
+    seats[row - 1][col - 1] = 'B'
+    draw()
 }
 
 fun price(row: Int): Int = if (rows * cols <= 60 || row <= rows / 2) 10 else 8
@@ -31,7 +41,8 @@ fun draw() {
     for (i in 1..rows) {
         print("$i ")
         for (j in 1..cols)
-            print("S ")
+            print("${seats[i - 1][j - 1]} ")
         println()
     }
+    println()
 }
